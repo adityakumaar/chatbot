a898497e22125e93f558dbad386c8219cf541ff0
Aditya Kumar <10adityakumar98@gmail.com>
2024-04-15 21:38:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,12 @@ from langchain.chains import ConversationalRetrievalChain, LLMChain
 #from langchain.chains import LLMChain
 from langchain_core.prompts import PromptTemplate
 
+# below 3 libraries are for loading remote models
+#from transformers import LlamaForCausalLM
+#from sentence_transformers import SentenceTransformer
+from transformers import AutoModelForCausalLM
+import torch
+
 
 # adding separator
 def add_vertical_space(spaces=1):
@@ -34,7 +40,7 @@ def main():
     TEMP_DIR = "temp"
 
     # embedding model path
-    EMBEDDING_MODEL_PATH = "embeddings/MiniLM-L6-v2"
+    #EMBEDDING_MODEL_PATH = "embeddings/MiniLM-L6-v2"
     
     # creating faiss db direcoty if it doesnot exist already
     if not os.path.exists(TEMP_DIR):
@@ -62,6 +68,9 @@ def main():
         data = loader.load()
 
         # creating embeddings using huggingface
+        #embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
+
+        # loading remote embedding model
         embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
         
         # creating chunks from CSV file
@@ -80,34 +89,23 @@ def main():
         # saving the vector store to local directory
         docsearch.save_local(DB_FAISS_PATH)
 
-        # loading local llama model
-        llm = CTransformers(#model="models/llama-2-7b-chat.ggmlv3.q8_0.bin",
-                            model="models/llama-2-7b-chat.ggmlv3.q4_0.bin",
-                            model_type="llama",
-                            #callbacks=[StreamingStdOutCallbackHandler()],
-                            config={'max_new_tokens': 1024,
-                                    'temperature': 0.5,
-                                    'context_length' : 4096
-                                    #'repetition_penalty': 1.1
-                                    }
-                            )
+        # loading remote llama model
+        #llm = LlamaForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf")
+        #llm = AutoModelForCausalLM.from_pretrained("google/gemma-1.1-2b-it")
 
-        # loading remote zephyr model
-        #llm = AutoModelForCausalLM.from_pretrained("TheBloke/zephyr-7B-beta-GGUF", 
-        #                                   model_file="zephyr-7b-beta.Q5_K_M.gguf", 
-        #                                   model_type="mistral", 
-        #                                   gpu_layers=50,
-        #                                   max_new_tokens = 1000,
-        #                                   context_length = 6000)
+        token = os.environ["HF_TOKEN"]
+        llm = AutoModelForCausalLM.from_pretrained(
+        "google/gemma-7b-it",
+        # torch_dtype=torch.float16 if torch.cuda.is_available() else torch.float32,
+        torch_dtype=torch.float16,
+        token=token,
+        )
+
         
-        # question answering chain
-        #memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
-
         # custom prompt
         custom_template="""
 You are a smart personal assistant and your task is to provide the answer of the given question based only on the given context. \n
 If you can't find the answer in the context, just say that "I don't know, please look up the policy." and don't try to make up an answer. \n\n
-
 Please, give the answer in plain english and don't repeat your answer and don't mention that you found the answer form the context and don't mention that the answer can be found in the context. \n
         
 Question: "{question}" \n\n
@@ -123,7 +121,7 @@ Helpful Answer:
         qa = ConversationalRetrievalChain.from_llm(llm,
                                                    #chain_type = "stuff",
                                                    chain_type = "stuff",
-                                                   verbose=True,
+                                                   #verbose=True,
                                                    #retriever=docsearch.as_retriever()
                                                    retriever=docsearch.as_retriever(search_kwargs = {"k" : 4, "search_type" : "similarity"}),
                                                    combine_docs_chain_kwargs={"prompt": QA_PROMPT}
